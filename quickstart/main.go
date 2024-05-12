dff95ab70c28606700a4f9663bf51e414c3f1256
EthanLeo <EthanLeo@yeah.net>
2024-05-13 00:47:07 +0800

    feat: 初始化quick star

--- quickstart/main.go
+++ quickstart/main.go
@@ -1,1 +1,17 @@
-package quickstart
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	// 创建一个server
+	server := gin.Default()
+	// 定义一个路由
+	server.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, "pong")
+	})
+	// 启动服务
+	server.Run(":8082")
+}
