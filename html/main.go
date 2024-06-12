193ea50aecce5f37437df96cbf4183029a11577b
EthanLeo <EthanLeo@yeah.net>
2024-06-12 22:31:19 +0800

    feat: 添加中间件定义、html渲染

--- html/main.go
+++ html/main.go
@@ -7,17 +7,20 @@ import (
 
 func main() {
 	server := gin.Default()
-	//server.LoadHTMLFiles("html/template/index.html", "html/template/index2.html")
+
 	server.LoadHTMLGlob("html/template/*")
 	server.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Hello world",
+			"title": "LoadHtmlFiles",
+			"name":  "名字",
 		})
 	})
+
 	server.GET("/index2", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index2.html", gin.H{
-			"title2": "Hello world2",
+			"title2": "title2",
 		})
 	})
+
 	server.Run(":8080")
 }
--- middleware/main.go
+++ middleware/main.go
@@ -8,23 +8,31 @@ import (
 
 func main() {
 	server := gin.Default()
-	v1 := server.Group("/v1")
-	v1.Use(func(c *gin.Context) {
-		fmt.Println("v1 ")
-		c.Next()
+	// 全局
+	server.Use(GlobalMiddleware, GlobalMiddleware2)
+	// 路由组
+	v1 := server.Group("/v1", UserGroupMiddleWareFirst)
+	// 路由
+	v1.GET("/index", PathMiddleware, func(c *gin.Context) {
+		// 具体业务逻辑
+		c.JSON(http.StatusOK, gin.H{
+			"data": "ok",
+		})
+	}, func(c *gin.Context) {
+		fmt.Println("after response")
 	})
 
-	userGroup := v1.Group("/users")
-	userGroup.Use(UserGroupMiddleWareFirst, UserGroupMiddleWareSecond)
-	userGroup.GET("/:id", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{})
+	v1.GET("/index2", func(c *gin.Context) {
+		// 具体业务逻辑
+		c.JSON(http.StatusOK, gin.H{
+			"data": "ok2",
+		})
 	})
-
 	server.Run(":8080")
 }
 
 func UserGroupMiddleWareFirst(c *gin.Context) {
-	fmt.Println("fist")
+	fmt.Println("group middle ware")
 	c.Next()
 }
 
@@ -32,4 +40,19 @@ func UserGroupMiddleWareSecond(c *gin.Context) {
 	fmt.Println("second")
 	c.Next()
 
+}
+
+func GlobalMiddleware(c *gin.Context) {
+	fmt.Println(fmt.Sprintf("global middle ware request path %s\n", c.Request.RequestURI))
+	c.Next()
+}
+
+func GlobalMiddleware2(c *gin.Context) {
+	fmt.Println(fmt.Sprintf("global middle2 ware request path %s\n", c.Request.RequestURI))
+	c.Next()
+}
+
+func PathMiddleware(c *gin.Context) {
+	fmt.Println("before response")
+	c.Next()
 }
